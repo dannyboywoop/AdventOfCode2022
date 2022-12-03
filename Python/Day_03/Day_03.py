b2a966d559756903b2bd6c90c36352e2c949b125
Daniel Holmes <dan@volantautonomy.com>
2022-12-03 12:31:51 +0000

    Day_03 star_02 python solution

--- Python/Day_03/Day_03.py
+++ Python/Day_03/Day_03.py
@@ -2,6 +2,7 @@ from aoc_tools import Advent_Timer
 
 ASCII_VAL_UPPER_OFFSET = 38
 ASCII_VAL_LOWER_OFFSET = 96
+GROUP_SIZE = 3
 
 
 def read_data(input_file="input.txt"):
@@ -27,8 +28,19 @@ def find_poorly_sorted_item(rucksack):
     return None
 
 
+def find_common_item(rucksacks):
+    return set.intersection(*(set(rucksack) for rucksack in rucksacks)).pop()
+
+
 def star_01(rucksacks):
     return sum(get_value(find_poorly_sorted_item(rucksack)) for rucksack in rucksacks)
+
+
+def star_02(rucksacks):
+    total = 0
+    for i in range(0, len(rucksacks), GROUP_SIZE):
+        total += get_value(find_common_item(rucksacks[i : i + GROUP_SIZE]))
+    return total
 
 
 if __name__ == "__main__":
@@ -41,6 +53,7 @@ if __name__ == "__main__":
     print(f"Star_01: {star_01(rucksacks)}")
     timer.checkpoint_hit()
 
+    print(f"Star_02: {star_02(rucksacks)}")
     timer.checkpoint_hit()
 
     timer.end_hit()
