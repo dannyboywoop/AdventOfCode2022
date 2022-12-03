63f43cb5e2fb21e1ba73987f465d06e4a0ba6bb2
Daniel Holmes <dan@volantautonomy.com>
2022-12-03 12:05:14 +0000

    Python Day_03 Star_01 solution

---
+++ Python/Day_03/Day_03.py
@@ -0,0 +1,46 @@
+from aoc_tools import Advent_Timer
+
+ASCII_VAL_UPPER_OFFSET = 38
+ASCII_VAL_LOWER_OFFSET = 96
+
+
+def read_data(input_file="input.txt"):
+    with open(input_file, "r") as file:
+        data = [line.strip() for line in file]
+    return data
+
+
+def get_value(char):
+    ascii_val = ord(char)
+    return ascii_val - (ASCII_VAL_UPPER_OFFSET if char.isupper() else ASCII_VAL_LOWER_OFFSET)
+
+
+def find_poorly_sorted_item(rucksack):
+    mid = len(rucksack) // 2
+    items_in_first_half = set(rucksack[:mid])
+
+    # for item in second half of rucksack
+    for item in rucksack[mid:]:
+        if item in items_in_first_half:
+            return item
+
+    return None
+
+
+def star_01(rucksacks):
+    return sum(get_value(find_poorly_sorted_item(rucksack)) for rucksack in rucksacks)
+
+
+if __name__ == "__main__":
+    timer = Advent_Timer()
+
+    rucksacks = read_data()
+    print("Input parsed!")
+    timer.checkpoint_hit()
+
+    print(f"Star_01: {star_01(rucksacks)}")
+    timer.checkpoint_hit()
+
+    timer.checkpoint_hit()
+
+    timer.end_hit()
