b1bca2f40f09b69ba8265f158719454b2b56056c
Daniel Tanno Holmes <dan@volantautonomy.com>
2022-12-06 12:37:06 +0000

    Slight speed improvement to Python day 6

--- Python/Day_06/Day_06.py
+++ Python/Day_06/Day_06.py
@@ -7,14 +7,14 @@ def read_data(input_file="input.txt"):
     return data
 
 
-def all_unique_chars(chars):
-    return len(chars) == len(set(chars))
-
-
 def find_start_of_packet_marker(data, marker_len=4):
     for i in range(marker_len, len(data) + 1):
-        chars = data[i - marker_len : i]
-        if all_unique_chars(chars):
+        # Rather than building a set each time, you could use a Counter and
+        # increment/decrement based on the moving window. That would be faster
+        # in theory, and is O(1) rather than O(N) w.r.t. marker_len, but in
+        # practice the overheads of the python Counter implementation eat up
+        # any speed gains at this scale of marker_len.
+        if len(set(data[i - marker_len : i])) == marker_len:
             return i
 
 
