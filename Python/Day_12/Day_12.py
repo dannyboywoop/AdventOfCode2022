2e81d396abefe0f63ffa4991e10caf4775a29a98
Daniel Tanno Holmes <dan@volantautonomy.com>
2022-12-15 17:23:19 +0000

    Day 12 python cleanup

--- Python/Day_12/Day_12.py
+++ Python/Day_12/Day_12.py
@@ -1,10 +1,10 @@
 from operator import add
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Tuple, TypeAlias
+from math import inf
+from queue import PriorityQueue
 
 from aoc_tools import Advent_Timer
-
-from shortest_path import find_best_path
 
 
 ORD_OFFSET = ord("a")
@@ -57,6 +57,27 @@ def prepare_vertices(char_map):
 def star_2(vertices, end):
     populate_neighbours(vertices, reverse=True)
     return find_best_path(vertices, end, lambda pos: vertices[pos].height == 0)
+
+
+def find_best_path(vertices, start_pos, goal_criteria):
+    open_set = PriorityQueue()
+    best_path_length_to_vertex = {start_pos: 0}
+
+    open_set.put((best_path_length_to_vertex[start_pos], start_pos))
+
+    while not open_set.empty():
+        current = open_set.get()[1]
+        if goal_criteria(current):
+            return best_path_length_to_vertex[current]
+
+        for neighbour in vertices[current].neighbours:
+            path_length_to_neighbour_via_current = best_path_length_to_vertex[current] + 1
+            if path_length_to_neighbour_via_current < best_path_length_to_vertex.get(
+                neighbour, inf
+            ):
+                best_path_length_to_vertex[neighbour] = path_length_to_neighbour_via_current
+                open_set.put((best_path_length_to_vertex[neighbour], neighbour))
+    return None
 
 
 if __name__ == "__main__":
