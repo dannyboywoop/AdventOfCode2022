c2c7068cdc9648666b2656e25ed8bb486bde8b35
Daniel Tanno Holmes <dan@volantautonomy.com>
2022-12-23 13:31:17 +0000

    Day_20 second star in python

--- Python/Day_20/Day_20.py
+++ Python/Day_20/Day_20.py
@@ -1,61 +1,53 @@
 from dataclasses import dataclass
 
+
 from aoc_tools import Advent_Timer
 
 
-class CircularDoubleLinkedList:
+class EncryptedData:
     @dataclass
     class Element:
         value: int
-        previous_element: int
-        next_element: int
+        address_table_key: int
 
     def __init__(self, array):
-        self.data = {
-            i: CircularDoubleLinkedList.Element(val, (i - 1) % len(array), (i + 1) % len(array))
-            for i, val in enumerate(array)
-        }
+        self.address_table = {i: i for i in range(len(data))}
+        self.current_data = [EncryptedData.Element(val, i) for i, val in enumerate(array)]
 
-    def find_relative_element(self, element, offset):
-        relative_element = element
-        if offset < 0:
-            for _ in range(1 - offset):
-                relative_element = self.data[relative_element].previous_element
-        else:
-            for _ in range(offset):
-                relative_element = self.data[relative_element].next_element
-        return relative_element
+    def calculate_new_idx(self, current_idx, offset):
+        return sum(divmod(current_idx + offset, len(self.current_data))) % (
+            len(self.current_data) - 1
+        )
 
-    def get_relative_element(self, element, offset):
-        return self.data[self.find_relative_element(element, offset)].value
+    def get_relative_element(self, address_table_key, offset):
+        return self.current_data[
+            (self.address_table[address_table_key] + offset) % len(self.current_data)
+        ].value
 
-    def pop(self, element):
-        previous_element = self.data[element].previous_element
-        next_element = self.data[element].next_element
-        self.data[previous_element].next_element = next_element
-        self.data[next_element].previous_element = previous_element
+    def move(self, original_idx):
+        # get current and new idx of element
+        current_idx = self.address_table[original_idx]
+        new_idx = self.calculate_new_idx(current_idx, self.current_data[current_idx].value)
 
-    def insert(self, element, position):
-        temp = self.data[position].next_element
-        self.data[position].next_element = element
-        self.data[element].previous_element = position
-        self.data[element].next_element = temp
-        self.data[temp].previous_element = element
-
-    def move(self, element, offset):
-        if offset == 0:
+        if new_idx == current_idx:
             return
-        self.pop(element)
-        new_position = self.find_relative_element(element, offset)
-        self.insert(element, new_position)
+
+        # move element in current_data, inefficient but life's too short
+        element = self.current_data.pop(current_idx)
+        self.current_data = self.current_data[:new_idx] + [element] + self.current_data[new_idx:]
+
+        # update element addresses
+        self.update_address_table()
+
+    def update_address_table(self):
+        for idx, element in enumerate(self.current_data):
+            self.address_table[element.address_table_key] = idx
 
     def __str__(self):
-        values = [self.data[0].value]
-        element = self.data[0].next_element
-        while element != 0:
-            values.append(self.data[element].value)
-            element = self.data[element].next_element
-        return ", ".join(str(value) for value in values)
+        return str([element.value for element in self.current_data])
+
+    def to_array(self):
+        return [element.value for element in self.current_data]
 
 
 def read_data(input_file="input.txt"):
@@ -65,14 +57,30 @@ def read_data(input_file="input.txt"):
 
 
 def star_1(data):
-    encrypted_data = CircularDoubleLinkedList(data)
+    encrypted_data = EncryptedData(data)
 
     # mix file
-    for i, val in enumerate(data):
-        encrypted_data.move(i, val)
+    for i in range(len(data)):
+        encrypted_data.move(i)
 
     # find grove coordinates
     zero_element = data.index(0)
+    return sum(
+        encrypted_data.get_relative_element(zero_element, offset) for offset in [1000, 2000, 3000]
+    )
+
+
+def star_2(data, decryption_key=811589153):
+    new_data = [val * decryption_key for val in data]
+    encrypted_data = EncryptedData(new_data)
+
+    # mix file 10 times
+    for _ in range(10):
+        for i in range(len(new_data)):
+            encrypted_data.move(i)
+
+    # find grove coordinates
+    zero_element = new_data.index(0)
     return sum(
         encrypted_data.get_relative_element(zero_element, offset) for offset in [1000, 2000, 3000]
     )
@@ -88,6 +96,7 @@ if __name__ == "__main__":
     print(f"Star_1: {star_1(data)}")
     timer.checkpoint_hit()
 
+    print(f"Star_2: {star_2(data)}")
     timer.checkpoint_hit()
 
     timer.end_hit()
