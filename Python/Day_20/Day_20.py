60aba474eb6380cd6c4720a21c7fef68e5158152
Daniel Holmes <dan@volantautonomy.com>
2022-12-23 13:53:03 +0000

    Day_20 speed improvement

--- Python/Day_20/Day_20.py
+++ Python/Day_20/Day_20.py
@@ -8,46 +8,39 @@ class EncryptedData:
     @dataclass
     class Element:
         value: int
-        address_table_key: int
+        address: int
 
     def __init__(self, array):
-        self.address_table = {i: i for i in range(len(data))}
-        self.current_data = [EncryptedData.Element(val, i) for i, val in enumerate(array)]
+        self.data = [EncryptedData.Element(val, i) for i, val in enumerate(array)]
 
-    def calculate_new_idx(self, current_idx, offset):
-        return sum(divmod(current_idx + offset, len(self.current_data))) % (
-            len(self.current_data) - 1
-        )
+    def calculate_new_address(self, current_address, offset):
+        return sum(divmod(current_address + offset, len(self.data))) % (len(self.data) - 1)
 
-    def get_relative_element(self, address_table_key, offset):
-        return self.current_data[
-            (self.address_table[address_table_key] + offset) % len(self.current_data)
-        ].value
+    def get_relative_element(self, original_idx, offset):
+        wanted_address = (self.data[original_idx].address + offset) % len(self.data)
+        for element in self.data:
+            if element.address == wanted_address:
+                return element.value
+        return None
 
     def move(self, original_idx):
-        # get current and new idx of element
-        current_idx = self.address_table[original_idx]
-        new_idx = self.calculate_new_idx(current_idx, self.current_data[current_idx].value)
+        # get current and new address of element
+        current_address = self.data[original_idx].address
+        new_address = self.calculate_new_address(current_address, self.data[original_idx].value)
 
-        if new_idx == current_idx:
+        if new_address == current_address:
             return
 
-        # move element in current_data, inefficient but life's too short
-        element = self.current_data.pop(current_idx)
-        self.current_data = self.current_data[:new_idx] + [element] + self.current_data[new_idx:]
-
-        # update element addresses
-        self.update_address_table()
-
-    def update_address_table(self):
-        for idx, element in enumerate(self.current_data):
-            self.address_table[element.address_table_key] = idx
-
-    def __str__(self):
-        return str([element.value for element in self.current_data])
-
-    def to_array(self):
-        return [element.value for element in self.current_data]
+        # update addresses changed by the move
+        for element in self.data:
+            if element.address < current_address:
+                if element.address >= new_address:
+                    element.address += 1
+            elif element.address > current_address:
+                if element.address <= new_address:
+                    element.address -= 1
+            else:
+                element.address = new_address
 
 
 def read_data(input_file="input.txt"):
